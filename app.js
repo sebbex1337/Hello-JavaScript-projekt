43997273ddcb7830c2423e17d77f53c0f711e0e2
sebbex1337 <sefort@hotmail.dk>
2023-01-31 10:12:13 +0100

    Changed to english

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hello " + document.querySelector("input").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
