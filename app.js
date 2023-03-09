56548386bac1e3a7a25b59f6101947da4e9e920c
sebbex1337 <sefort@hotmail.dk>
2023-03-09 09:52:51 +0100

    exercises

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
+"use strict"
+
+let fullName;
+let age;
+let mail;
+
 function sayHello() {
-  document.querySelector("#h3name").textContent =
-    "Hello " + document.querySelector("#name").value;
-  document.querySelector("#h3age").textContent =
-    "Alder: " + document.querySelector("#age").value;
-  document.querySelector("#h3email").textContent =
-    "Email: " + document.querySelector("#email").value;
+  fullName = document.querySelector("#name").value;
+  age = document.querySelector("#age").value;
+  mail = document.querySelector("#email").value;
+
+  document.querySelector("#output").textContent = `Hej ${fullName}, alder: ${age}. Mail: ${mail}`;
 }
 document.querySelector("button").addEventListener("click", sayHello);
 
-/* 
-function sayHello2() {
-  document.getElementById("output").textContent =
-    "Hello " + document.getElementById("field").value;
-}
-document.getElementById("btn").addEventListener("click", sayHello2);
- */
